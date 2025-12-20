d6d38412177028e9bab56ca4fbe60d1d37d57635
Adam Radvinsky <radvinskyadam@gmail.com>
2025-12-19 20:55:32 -0800

    dragging
    you can now drag shapes

--- Assets/Scripts/shape.cs
+++ Assets/Scripts/shape.cs
@@ -1,16 +1,35 @@
+using System;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class shape : MonoBehaviour
 {
+
+    public float test;
+   // public Transform transform;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Debug.Log("bruh ");
+
+        //position = transform.GetComponent<Transform>();
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+
     }
+
+
+    void OnMouseDrag()
+    {
+
+        Vector2 drag_position= Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        transform.position = drag_position;
+    }
+
 }
